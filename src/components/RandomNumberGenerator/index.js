7cf7e8ac0eac2c6b883249281c42c6d1c375f43b
Nandhakumar055 <Nandhakumar055.githup.com>
2023-12-21 11:05:30 +0000

    first commit

---
+++ src/components/RandomNumberGenerator/index.js
@@ -0,0 +1,33 @@
+import {Component} from 'react'
+import './index.css'
+
+class RandomNumberGenerator extends Component {
+  state = {randomNumber: 0}
+
+  generateNumber = () => {
+    this.setState({randomNumber: Math.ceil(Math.random() * 100)})
+  }
+
+  render() {
+    const {randomNumber} = this.state
+
+    return (
+      <div className="bg-container">
+        <h1 className="heading">Random Number</h1>
+        <p className="note-message">
+          Generate a random number in the range of 0 to 100
+        </p>
+        <button
+          type="button"
+          onClick={this.generateNumber}
+          className="generate-button"
+        >
+          Generate
+        </button>
+        <p className="generate-number-text">{randomNumber}</p>
+      </div>
+    )
+  }
+}
+
+export default RandomNumberGenerator
